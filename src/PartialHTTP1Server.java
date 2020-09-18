1628030cd3b561ba621b81c59c74527f5e0821ed
EshanWadhwa <wadhwaeshan@gmail.com>
2020-09-18 14:18:49 -0400

    server

--- src/PartialHTTP1Server.java
+++ src/PartialHTTP1Server.java
@@ -1,9 +1,46 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class PartialHTTP1Server {
+public class PartialHTTP1Server implements Runnable {
 
-	public static void main(String[] args) {
+	private Socket connect;
+
+	static final int PORT = 8000;
+
+	public PartialHTTP1Server(Socket c) {
+		connect = c;
+	}
+
+
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+
+		try {
+
+
+		ServerSocket serverConnect = new ServerSocket(PORT);
+
+		System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
+
+		while (true) {
+
+			PartialHTTP1Server myServer = new PartialHTTP1Server(serverConnect.accept());
+
+			Thread thread = new Thread(myServer);
+			thread.start();
+
+
+		}
+
+		} catch (IOException e) {
+			System.err.println("Server Connection error : " + e.getMessage());
+		}
 
 	}
 
+	@Override
+	public void run() {
+
+	}
 }
