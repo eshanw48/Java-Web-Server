4df3a4a14f6b2594568fa12dd78578bfbd6ecbcd
EshanWadhwa <wadhwaeshan@gmail.com>
2020-09-21 20:40:44 -0400

    server client working with threads

--- src/PartialHTTP1Server.java
+++ src/PartialHTTP1Server.java
@@ -3,28 +3,61 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.StringTokenizer;
 
-public class PartialHTTP1Server{
+public class PartialHTTP1Server implements Runnable{
+
+	private Socket connect;
 
 	static final int PORT = 8000;
 
+	public PartialHTTP1Server(Socket c) {
+		connect = c;
+	}
+
 	public static void main(String argv[]) throws Exception {
-		String clientSentence;
-		String capitalizedSentence;
+
 		try {
-		ServerSocket welcomeSocket = new ServerSocket(PORT);
-		System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
+	//	ServerSocket welcomeSocket = new ServerSocket(PORT);
+	//	System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
+
+			ServerSocket serverConnect = new ServerSocket(PORT);
+
+			System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
 
 		while (true) {
 
-			Socket connectionSocket = welcomeSocket.accept();
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-			clientSentence = inFromClient.readLine();
-			capitalizedSentence = clientSentence.toUpperCase() + '\n';
-			outToClient.writeBytes(capitalizedSentence);
+
+
+
+			PartialHTTP1Server myServer = new PartialHTTP1Server(serverConnect.accept());
+
+			Thread thread = new Thread(myServer);
+			thread.start();
+
+			//Socket connectionSocket = welcomeSocket.accept();
+
 		}
 		} catch (Exception e) {
 			System.err.println("Server Connection error : " + e.getMessage());
 		}
+	}
+
+	@Override
+	public void run() {
+		String clientSentence;
+		String capitalizedSentence;
+		try{
+
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connect.getOutputStream());
+			clientSentence = inFromClient.readLine();
+			capitalizedSentence = clientSentence.toUpperCase() + '\n';
+			outToClient.writeBytes(capitalizedSentence);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+
 	}
 }
