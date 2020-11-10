3c2f6725046a965f350e360ad5f5019ce07e4088
Shivam Patel <46139029+Shivam1615@users.noreply.github.com>
2020-11-09 22:37:18 -0500

    Update PartialHTTP1Server.java

--- src/PartialHTTP1Server.java
+++ src/PartialHTTP1Server.java
@@ -59,21 +59,21 @@ public class PartialHTTP1Server implements Runnable{
 			ServerSocket serverConnect = new ServerSocket(Integer.parseInt(args[0]));
 			System.out.println("Server started.\nListening for connections on port : " + Integer.parseInt(args[0])+ " ...\n");
 			pool = new ThreadPoolExecutor(5, 50,
-					60L, TimeUnit.SECONDS,
+					0, TimeUnit.SECONDS,
 					new SynchronousQueue<Runnable>());
 			// We listen until user ends server execution.
 			while (true) {
 				PartialHTTP1Server myServer = new PartialHTTP1Server(serverConnect.accept());
 
 				if (verbose) {
-					System.out.println("Connecton opened. (" + new Date() + ")");
+					//System.out.println("Connecton opened. (" + new Date() + ")");
 				}
 
-				System.out.println("Pool Size:" + pool.getPoolSize());
+				//System.out.println("Pool Size:" + pool.getPoolSize());
 
 				// We manage the threads here.
 				if(pool.getPoolSize()>=50){
-					System.out.println("Service Unavailable");
+					//System.out.println("Service Unavailable");
 					PrintWriter out = new PrintWriter(myServer.connect.getOutputStream());
 					out.println("HTTP/1.0 503 Service Unavailable\r");
 					out.println("\r");
@@ -105,6 +105,7 @@ public class PartialHTTP1Server implements Runnable{
 		while ((line = IN.readLine()) != null && !line.isEmpty()) {
 			sb.append(line).append("\r\n");
 		}
+		//System.out.println(sb.toString());
 		return sb.toString();
 	}
 
@@ -113,9 +114,10 @@ public class PartialHTTP1Server implements Runnable{
 	 */
 	@Override
 	public void run() {
+
 		long opened = System.currentTimeMillis();
 
-		 PrintWriter out = null; BufferedOutputStream dataOut = null;
+		PrintWriter out = null; BufferedOutputStream dataOut = null;
 		String fileRequested = null;
 
 
@@ -135,17 +137,15 @@ public class PartialHTTP1Server implements Runnable{
 			String request;
 			String request2;
 			String lines[] = header.split("\r?\n");
-			if(lines.length==2){
-				request=lines[0];
-				request2=lines[1];
-			}else if(lines.length==1){
-				request=lines[0];
-			}else{
+
+			if(lines.length == 0){
 				out.println("HTTP/1.0 400 Bad Request\r");
 				out.println("\r");
 				out.flush();
 				return;
 			}
+			request = lines[0];
+
 
 			long received = System.currentTimeMillis();
 			float sec = (received-opened)/ 1000F;
@@ -155,7 +155,7 @@ public class PartialHTTP1Server implements Runnable{
 			}
 
 			// We parse the request with a string tokenizer.
-			System.out.println(header);
+			//System.out.println(header);
 			StringTokenizer parse = new StringTokenizer(request);
 
 			// Request most have 3 parts.
@@ -180,10 +180,10 @@ public class PartialHTTP1Server implements Runnable{
 				try
 				{
 
-				 if(version.charAt(i) == '/')
-				{
-					num = Double.parseDouble(version.substring(i+1));
-				}
+					if(version.charAt(i) == '/')
+					{
+						num = Double.parseDouble(version.substring(i+1));
+					}
 				}
 				catch(Exception e)
 				{
@@ -193,12 +193,12 @@ public class PartialHTTP1Server implements Runnable{
 
 					return;
 
-			}
+				}
 			}
 			//If http version > 1.0, then the version is not supported.
-			 if(num > 1.0)
+			if(num > 1.0)
 			{
-				System.out.println(num);
+				//System.out.println(num);
 				out.println("HTTP/1.0 505 HTTP Version Not Supported\r");
 				out.println("\r");
 				out.flush();
@@ -212,7 +212,7 @@ public class PartialHTTP1Server implements Runnable{
 				// Not implemented requests.
 				if(method.equals("PUT") || method.equals("DELETE") || method.equals("LINK") || method.equals("UNLINK")) {
 					if (verbose) {
-						System.out.println("501 Not Implemented : " + method + " method.");
+						//System.out.println("501 Not Implemented : " + method + " method.");
 					}
 
 
@@ -246,7 +246,7 @@ public class PartialHTTP1Server implements Runnable{
 
 
 				//If method is GET or POST.
-				if (method.equals("GET") || method.equals("POST")) {
+				if (method.equals("GET")) {
 					if(!file.canRead())
 					{
 						out.println("HTTP/1.0 403 Forbidden\r");
@@ -266,7 +266,7 @@ public class PartialHTTP1Server implements Runnable{
 					DateFormat formatter=new SimpleDateFormat("E,dd MMM yyyy HH:mm:ss");
 
 
-					if(lines.length==2) {
+					if(lines.length > 1) {
 						String mod="";
 						for (int i = 0; i < lines[1].length(); i++) {
 							if (lines[1].charAt(i) == ':' ) {
@@ -275,7 +275,7 @@ public class PartialHTTP1Server implements Runnable{
 							}
 						}
 						mod=mod.trim();
-						System.out.println(mod + " is a different mod");
+						//System.out.println(mod + " is a different mod");
 						Date date1 = null;
 						try {
 							date1 = formatter.parse(mod);
@@ -326,6 +326,133 @@ public class PartialHTTP1Server implements Runnable{
 					dataOut.flush();
 				}
 
+				if(method.equals("POST")){
+
+					if(!file.canRead())
+					{
+						out.println("HTTP/1.0 403 Forbidden\r");
+						out.println("\r");
+						out.flush();
+
+						return;
+
+					}
+
+					System.out.println(fileRequested);
+					if(!fileRequested.endsWith(".cgi")){
+						out.println("HTTP/1.0 405 Method Not Allowed\r");
+						out.println("\r");
+						out.flush();
+						return;
+					}
+
+					boolean content_type = false;
+					boolean content_length = false;
+
+					for(int i = 1; i < lines.length; i++){
+						StringTokenizer parse_temp = new StringTokenizer(lines[i]);
+						if(parse_temp.countTokens() != 2){
+							out.println("HTTP/1.0 400 Bad Request\r");
+							out.println("\r");
+							out.flush();
+							return;
+						}
+							String token1 = parse_temp.nextToken();
+							String token2 = parse_temp.nextToken();
+							if(token1.equals("Content-Length:")){
+								try{
+									Integer.parseInt(token2);
+									content_length = true;
+								}catch(Exception e){
+
+								}
+							}else if(token1.equals("Content-Type:")){
+								content_type = true;
+							}
+					}
+
+					if(!content_type){
+						out.println("HTTP/1.0 500 Internal Server Error\r");
+						out.println("\r");
+						out.flush();
+						return;
+					}if(!content_length){
+						out.println("HTTP/1.0 411 Length Required\r");
+						out.println("\r");
+						out.flush();
+						return;
+					}
+
+
+					byte[] fileData = readFileData(file, fileLength);
+					long lastModified = file.lastModified();
+					Date modified  = new Date(lastModified);
+					DateFormat converter2 = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss");
+					converter2.setTimeZone(TimeZone.getTimeZone("GMT"));
+					DateFormat formatter=new SimpleDateFormat("E,dd MMM yyyy HH:mm:ss");
+
+
+					if(lines.length==2) {
+						String mod="";
+						for (int i = 0; i < lines[1].length(); i++) {
+							if (lines[1].charAt(i) == ':' ) {
+								mod = lines[1].substring(i+1);
+								break;
+							}
+						}
+						mod=mod.trim();
+						//System.out.println(mod + " is a different mod");
+						Date date1 = null;
+						try {
+							date1 = formatter.parse(mod);
+							formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+							if(modified.compareTo(date1)<0){
+								out.println("HTTP/1.0 304 Not Modified\r");
+								Calendar calendar = Calendar.getInstance();
+								calendar.add(Calendar.YEAR, 1);
+								Date tomorrow = calendar.getTime();
+								DateFormat converter3 = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss");
+								converter3.setTimeZone(TimeZone.getTimeZone("GMT"));
+								out.println("Expires: " + converter3.format(tomorrow) + " GMT\r");
+								out.println("\r");
+								out.flush();
+
+								return;
+							}
+						} catch(ParseException ioe){
+							//System.out.println("Is not a valid date");
+						}
+
+					}
+
+
+
+					out.println("HTTP/1.0 200 OK\r");
+					Date localtime = new Date();
+					DateFormat converter = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss");
+					converter.setTimeZone(TimeZone.getTimeZone("GMT"));
+					out.println("Date: " + converter.format(localtime) + " GMT\r");
+					out.println("Server: Apache/1.3.27 (Unix)\r");
+					out.println("MIME-version: 1.0\r");
+					out.println("Last-Modified: " + converter2.format(modified) + " GMT\r");
+					out.println("Content-Type: " + content + "\r");
+					out.println("Content-Length: " + fileLength + "\r");
+					out.println("Content-Encoding: identity\r");
+					out.println("Allow: GET, POST, HEAD\r");
+					Calendar calendar = Calendar.getInstance();
+					calendar.add(Calendar.YEAR, 1);
+					Date tomorrow = calendar.getTime();
+					DateFormat converter3 = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss");
+					converter.setTimeZone(TimeZone.getTimeZone("GMT"));
+					out.println("Expires: " + converter3.format(tomorrow) + " GMT\r");
+					out.println("\r");
+					out.flush();
+
+					dataOut.write(fileData, 0, fileLength);
+					dataOut.flush();
+
+				}
+
 				// If method is HEAD.
 				if(method.equals("HEAD"))
 				{
@@ -371,7 +498,7 @@ public class PartialHTTP1Server implements Runnable{
 				}
 
 				if (verbose) {
-					System.out.println("File " + fileRequested + " of type " + content + " returned");
+					//System.out.println("File " + fileRequested + " of type " + content + " returned");
 				}
 
 			}
@@ -399,7 +526,7 @@ public class PartialHTTP1Server implements Runnable{
 			}
 
 			if (verbose) {
-				System.out.println("Connection closed.\n");
+				//System.out.println("Connection closed.\n");
 			}
 		}
 
@@ -461,7 +588,7 @@ public class PartialHTTP1Server implements Runnable{
 		{
 			return "application/x-gzip";
 		}
-		else if(fileRequested.endsWith("ls"))
+		else if(fileRequested.endsWith("ls") || fileRequested.endsWith(".cgi"))
 		{
 			return "application/octet-stream";
 		}
